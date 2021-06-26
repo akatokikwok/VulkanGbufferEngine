d575e81e17f4d31b962fce434d8c85ddabfd27da
CHINA\gWX1027342 <akatokikwok@outlook.com>
2021-06-26 17:42:18 +0800

    [feat]: 绘制一个基础三角形,把4个事件进行设计

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,14 @@
 LRESULT CALLBACK LearnWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message) {
-		case WM_CLOSE: // 当鼠标点击关闭按钮的时候,事件会被识别为WM_CLOSE型
+		case WM_SIZE: {// 当窗口尺寸改变时候,事件会被识别为WM_SIZE型
+			RECT rect;
+			GetClientRect(hwnd, &rect);// 拿取 显示视口(不是整个窗口)的大小
+			OnViewportChanged(rect.right - rect.left, rect.bottom - rect.top);// 调用重适配视口,注意是buttom - top
+		} break;
+		case WM_CLOSE: {// 当鼠标点击关闭按钮的时候,事件会被识别为WM_CLOSE型
 			PostQuitMessage(0);// 当消息为WM_CLOSE型就退出循环
-			break;
+		} break;
 	}
 	return DefWindowProc(hwnd, message, wParam, lParam);// 没有消息事件处理时,就使用默认的处理
 }
@@ -30,32 +35,33 @@ INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	wndclassex.lpszClassName = L"BattleFireWindow"; // 加L把字符从char转换为WChar型,注意使用Unicode字符集
 	wndclassex.style = CS_VREDRAW | CS_HREDRAW;// 绘制时采用水平重绘和垂直重绘
 
-	RECT rect = { 0, 0, 1280, 720 };// 设置一个窗口不包含边框的内容大小,不包含边框的显示区域为1280*720,若包含了边框则需要下一行的方法自动调节计算
-	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
-
 	ATOM atom = RegisterClassEx(&wndclassex);// 注册窗口,返回ATOM型
 	if (atom == 0) {
 		MessageBox(nullptr, L"窗口内容:窗口注册失败", L"窗口标题:错误", MB_OK);
 		return -1;
 	}
+
+	RECT rect = { 0, 0, 1280, 720 };// 设置一个窗口不包含边框的内容大小,不包含边框的显示区域为1280*720,若包含了边框则需要下一行的方法自动调节计算
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
 	/* 创建窗口*/
 	HWND hwnd = CreateWindowEx(NULL, L"BattleFireWindow", L"LearnWindow", WS_OVERLAPPEDWINDOW, 
 		0, 0,/*窗口左上角坐标*/ rect.right - rect.left, rect.bottom - rect.top,
 		nullptr,/*父窗口句柄*/ nullptr,/*菜单*/ hInstance, nullptr/*创建窗口时额外的参数*/
 	);
-	ShowWindow(hwnd, SW_SHOW);
 
 	/* 初始化VULKAN环境*/
 	InitVulkan(hwnd, 1280, 720);
 	/* 调用场景初始化函数*/
 	Init();
-
+	/* 呈现窗口句柄*/
+	ShowWindow(hwnd, SW_SHOW);
 	UpdateWindow(hwnd);// 显示窗口后再刷新成要的颜色
 
 	MSG msg;//要存放消息的结构体
 	float last_time = timeGetTime() / 1000.0f;// 返回计数,秒
 	while (true) {
-		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {// 获取所有消息后再把它们从队列中移除掉
+		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {// 持续获取所有消息后再把它们从队列中移除掉
 			if (msg.message == WM_QUIT) {// 若获取到的消息类型识别为WM_QUIT就中断循环
 				break;
 			}
--- scene.cpp
+++ scene.cpp
@@ -1,22 +1,60 @@
 ﻿#include "scene.h"
 #include "BVulkan.h"
 
+AVulkanHandle program;
+AVulkanHandle vbo;
 void Init()
 {
+	// 目前使用的都是NDC空间坐标, 逆时针绘制
+	Vertex vertexes[3];
+	vertexes[0].SetPosition(-0.5f, 0.5f, 0.0f);
+	vertexes[0].SetTexcoord(1.0f, 0.0f, 1.0f, 1.0f);
+	vertexes[1].SetPosition(0.5f, 0.5f, 0.0f);
+	vertexes[1].SetTexcoord(1.0f, 1.0f, 0.0f, 1.0f);
+	vertexes[2].SetPosition(0.0f, -0.5f, 0.0f);
+	vertexes[2].SetTexcoord(0.0f, 1.0f, 1.0f, 1.0f);
+	vbo = glGenBuffer();
+	glBufferData(vbo, sizeof(Vertex) * 3, vertexes);// 构建点集数据
+	program = aCreateProgram();// 创建应用
 
+	GLuint vs, fs;
+	int file_len = 0;
+	unsigned char* file_content = LoadFileContent("Res/test.vsb", file_len);// 先加载这个二进制顶点shader
+	aCreateShader(vs, file_content, file_len);// 创建出顶点shader
+	delete[]file_content;// 用完这个VSshader就可以把文件删了
+	file_content = LoadFileContent("Res/test.fsb", file_len);// 重用资源, 再加载这个二进制像素shader
+	aCreateShader(fs, file_content, file_len);// 创建出像素shader
+	delete[]file_content;// 用完这个FSshader就可以把文件删了
+
+	// 各自绑到管线上 并最终链接程序
+	aAttachVertexShader(program, vs);
+	aAttachFragmentShader(program, fs);
+	aLinkProgram(program);
 }
 
 void Draw(float deltaTime)
 {
+	aClearColor(0.1f, 0.4f, 0.6f, 1.0f);
 
+	VkCommandBuffer commandbuffer = aBeginRendering(); /*******开始绘制标志******/
+	glUseProgram(program);
+	glBindVertexBuffer(vbo);
+	glDrawArrays(A_TRIANGLES/*图元类型*/, 0/*数据偏移*/, 3/*欲绘制的点个数*/);
+	aEndRenderingCommand();/*******结束绘制标志******/
+	aSwapBuffers();// 绘制完就交换前后缓冲
 }
 
 void OnViewportChanged(int width, int height)
 {
-
+	aViewport(width, height);// 当拖动窗口或者其他改变尺寸操作时, 就需要重适配视口
 }
 
 void OnQuit()
 {
-
+	if (program != nullptr) {
+		aDeleteProgram(program);
+	}
+	if (vbo != nullptr) {
+		glDeleteBufferObject(vbo);
+	}
 }
